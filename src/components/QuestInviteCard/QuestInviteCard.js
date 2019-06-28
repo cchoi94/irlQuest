0d897bad923ae90c8dacc59f452152369c26abbe
Cody Choi <cchoi_2294@hotmail.com>
2019-06-28 01:12:25 -0400

    [QuestInviteCard] Tweak media quries and responsiveness, level style can be dynamic

--- src/components/QuestInviteCard/QuestInviteCard.js
+++ src/components/QuestInviteCard/QuestInviteCard.js
@@ -4,6 +4,8 @@ import axios from '../Requests/FirebaseInstance'
 import classes from './QuestInviteCard.module.scss'
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+
 
 const QuestInviteCard = (props) => {
 
@@ -12,29 +14,39 @@ const QuestInviteCard = (props) => {
   const fetchRewards = () => {
     return data.reward.map(reward => {
       return (
-          <p key={reward.id} className={"desktopDescription"}>x{reward.amount} {reward.name}</p>
+          <p key={reward.id} className={"description"}>x{reward.amount} {reward.name}</p>
       )
     })
   }
 
+  const mobile = useMediaQuery('(max-width:1024px)');
+
+  const levelSubheaderStyle = {
+    color: '#FFB300'
+  }
+
   return(
     <Paper className={classes.QuestInviteCardContainer}>
-      <img src={data.image} alt="Kensington Market Cover" className={classes.QuestInviteCoverImage}/>
-      <div className={classes.QuestInfoContainer}>
-        <p className={"desktopHeader"}>
+      {mobile ? 
+       <img src={data.imageMobile} alt="Kensington Market Cover" className={classes.QuestInviteCoverImage}/>
+      :
+      <img src={data.imageDesktop} alt="Kensington Market Cover" className={classes.QuestInviteCoverImage}/>
+      }
+      <div className={"infoContainer"}>
+        <p className={"header"}>
           {data.name}
         </p>
         <div className={`${classes.SubheaderContainer} ${classes.QuestInviteCardSubheaderContainer}`}>
-          <span className={"desktopSubheader"}>{data.location.name}</span>
-          <span className={"desktopSubheader"}>Difficulty {data.difficulty}/5</span>
+          <span className={"subheader"}>{data.location.name} | </span>
+          <span className={`subheader`} style={levelSubheaderStyle}>Level {data.level}</span>
         </div>
         <div className={"paragraphContainer"}>
-          <p className={"desktopDescription"}>
+          <p className={"description"}>
             {data.description}
           </p>
         </div>
         <div className={classes.RewardContainer}>
-          <span className={"desktopSubheader"}>Reward</span>
+          <span className={"subheader"}>Reward</span>
           <div className={classes.RewardsList}>
             {fetchRewards()}
           </div>
